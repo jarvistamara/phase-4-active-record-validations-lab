e0678bfff34902f3c3fd87173e948cde2c508028
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-19 00:53:37 -0400

    commit tree a2246153ef10edea94138cdb63c524f1b699f6ee

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+    def clickbait
+        accepted_titles = [ "Won't Believe", "Secret",  "Top [number]",  "Guess" ]
+        if title
+            title_check = accepted_titles.any? {|t| title.include?(t)}
+            if !title_check
+                errors.add(:title, "need to be clickbait-y")
+            end
+        end
+    end
 end
